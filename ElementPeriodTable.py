addc7f502b6d1b8a5b3283d2f4f9102dd62b4565
Koan <koan_wong@icloud.com>
2026-01-20 14:52:51 +0800

    Remove unused OpenGL setup and simplify materials

--- ElementPeriodTable.py
+++ ElementPeriodTable.py
@@ -1,14 +1,11 @@
 import sys
 
 from PyQt5 import QtWidgets
-from PyQt5.QtOpenGL import QGLFormat
 
 from main_window import MainWindow
 
 
 def main() -> None:
-    fmt = QGLFormat()
-    fmt.setSampleBuffers(True)
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
